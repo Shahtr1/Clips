3d2fd121182c64fbf11532f9934c166583600852
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-05-26 16:56:05 +0530

    Added icon to handle video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,4 +5,25 @@ import { AfterViewInit, Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  isMusicPlaying = false;
+
+  musicControlButton?: HTMLVideoElement;
+
+  ngAfterViewInit(): void {
+    this.musicControlButton = document.getElementById(
+      'videoElement'
+    ) as HTMLVideoElement;
+  }
+
+  play() {
+    this.isMusicPlaying = !this.isMusicPlaying;
+    if (this.musicControlButton) this.musicControlButton.play();
+  }
+
+  pause() {
+    this.isMusicPlaying = !this.isMusicPlaying;
+
+    if (this.musicControlButton) this.musicControlButton.pause();
+  }
+}
